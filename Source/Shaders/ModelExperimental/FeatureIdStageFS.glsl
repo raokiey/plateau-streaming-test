3f77083f608c2aaba4f582dcfbf4325a16c599db
pikkarin <raokiey@gmail.com>
2022-04-09 19:15:09 +0900

    remove extra file

--- Source/Shaders/ModelExperimental/FeatureIdStageFS.glsl
+++
@@ -1,3 +0,0 @@
-void featureIdStage(out FeatureIds featureIds, ProcessedAttributes attributes) {
-  initializeFeatureIds(featureIds, attributes);
-}
--- Source/Shaders/ModelExperimental/FeatureIdStageVS.glsl
+++
@@ -1,5 +0,0 @@
-void featureIdStage(out FeatureIds featureIds, ProcessedAttributes attributes) 
-{
-  initializeFeatureIds(featureIds, attributes);
-  setFeatureIdVaryings();
-}
